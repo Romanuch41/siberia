1c08d23e70c301c51012dde154b4e736370bd072
Romanuch41 <pfonit@mail.ru>
2025-12-16 14:59:54 +0800

    added new element

---
+++ SiberiaApp/Classes/Elements/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiberiaApp.Classes.Elements
+{
+    public partial class Card
+    {
+        string name;
+        string price;
+        string date;
+
+        public Card(string name, string price, string date)
+        {
+            this.name = name;
+            this.price = price;
+            this.date = date;
+        }
+    }
+}
